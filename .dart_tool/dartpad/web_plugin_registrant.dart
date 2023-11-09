2fd72b8552f88903eefde32bfd957ba5c897a689
glenrynaldyhermanus <61014001+glenrynaldyhermanus@users.noreply.github.com>
2023-11-09 23:36:32 +0700

    push

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,14 +6,11 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
-import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:app_links/app_links_web.dart';
 import 'package:file_picker/_internal/file_picker_web.dart';
-import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_performance_web/firebase_performance_web.dart';
-import 'package:firebase_storage_web/firebase_storage_web.dart';
 import 'package:flutter_native_splash/flutter_native_splash_web.dart';
-import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:sign_in_with_apple_web/sign_in_with_apple_web.dart';
@@ -23,14 +20,11 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
-  FirebaseFirestoreWeb.registerWith(registrar);
+  AppLinksPluginWeb.registerWith(registrar);
   FilePickerWeb.registerWith(registrar);
-  FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   FirebasePerformanceWeb.registerWith(registrar);
-  FirebaseStorageWeb.registerWith(registrar);
   FlutterNativeSplashWeb.registerWith(registrar);
-  GoogleSignInPlugin.registerWith(registrar);
   ImagePickerPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   SignInWithApplePlugin.registerWith(registrar);
